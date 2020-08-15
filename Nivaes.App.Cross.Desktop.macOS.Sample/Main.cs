5bc636de13dc7a0b9f1b0d70abe8bad6e2ed9bf9
Andrés Giménez Muñoz <andres.gimenez@outlook.com>
2020-08-15 19:07:18 +0200

    Include macOS Sample.

---
+++ Nivaes.App.Cross.Desktop.macOS.Sample/Main.cs
@@ -0,0 +1,14 @@
+﻿using AppKit;
+
+namespace Nivaes.App.Desktop.macOS.Sample
+{
+    static class MainClass
+    {
+        static void Main(string[] args)
+        {
+            NSApplication.Init();
+            NSApplication.Main(args);
+        }
+    }
+}
+
