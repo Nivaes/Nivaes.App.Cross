4d94d9db89807c0deaede07381b745438a31f1ef
Andres Gimenez Muñoz <andres.gimenez@outlook.com>
2020-08-15 19:51:09 +0200

    Create Watch samples.

---
+++ Samples/Nivaes.App.Cross.Watch.watchOS.Sample/Nivaes.App.Cross.Watch.watchOS.Sample.WatchOSExtension/ExtensionDelegate.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+using Foundation;
+using WatchKit;
+
+namespace Nivaes.App.Cross.Watch.watchOS.Sample.WatchOSExtension
+{
+    public class ExtensionDelegate : WKExtensionDelegate
+    {
+        public ExtensionDelegate()
+        {
+        }
+    }
+}
+
