4c8559ebd80479a1303175ad7c807e4d8b2c4cb2
Andres Gimenez Muñoz <andres.gimenez@outlook.com>
2020-08-15 19:37:43 +0200

    Move samples.

---
+++ Samples/Nivaes.App.Cross.Desktop.Sample/Components/AppMobileSampleAppStart.cs
@@ -0,0 +1,23 @@
+﻿namespace Nivaes.App.Desktop.Sample
+{
+    using System;
+    using System.Threading.Tasks;
+    using MvvmCross.Navigation;
+    using MvvmCross.ViewModels;
+    using Nivaes.App.Sample;
+
+    public sealed class AppMobileSampleAppStart
+        : AppSampleAppStart, IMvxAppStart
+    {
+        public AppMobileSampleAppStart(IMvxApplication application, IMvxNavigationService navigationService)
+            : base(application, navigationService)
+        {
+        }
+
+        protected override Task NavigateToFirstViewModel(object? hint = null)
+        {
+            //return base.NavigationService.Navigate<InitializingAppViewModel>();
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Samples/Nivaes.App.Cross.Desktop.Sample/Components/AppMobileSampleApplication.cs
@@ -0,0 +1,11 @@
+﻿namespace Nivaes.App.Desktop.Sample
+{
+    using MvvmCross.ViewModels;
+    using Nivaes.App.Sample;
+
+    public sealed class AppMobileSampleApplication<TType>
+        : AppSampleApplication<TType>
+          where TType : class, IMvxAppStart
+    {
+    }
+}
