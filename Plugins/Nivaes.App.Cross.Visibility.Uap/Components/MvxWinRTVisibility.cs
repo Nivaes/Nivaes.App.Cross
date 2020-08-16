c7a43f9c5dc444c33bcb3d9b3c0c8799d89e9ef5
Andres Gimenez Muñoz <andres.gimenez@outlook.com>
2020-08-16 21:59:27 +0200

    Projects.

--- Plugins/Nivaes.App.Cross.Visibility.Uap/Components/MvxWinRTVisibility.cs
+++ Plugins/Nivaes.App.Cross.Visibility.Uap/Components/MvxWinRTVisibility.cs
@@ -2,11 +2,11 @@
 // The .NET Foundation licenses this file to you under the MS-PL license.
 // See the LICENSE file in the project root for more information.
 
-using MvvmCross.UI;
-using UapVisibility = Windows.UI.Xaml.Visibility;
-
 namespace MvvmCross.Plugin.Visibility.Platforms.Uap
 {
+    using MvvmCross.UI;
+    using UapVisibility = Microsoft.UI.Xaml.Visibility;
+
     public class MvxWinRTVisibility : IMvxNativeVisibility
     {
         public object ToNative(MvxVisibility visibility)
