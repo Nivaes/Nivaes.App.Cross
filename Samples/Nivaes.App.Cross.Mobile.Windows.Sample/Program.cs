84c75d96333fe19f75844f9c2866c3f7499b28d3
Andres Gimenez Muñoz <andres.gimenez@outlook.com>
2020-11-18 15:33:03 +0100

    Update to net5.0-windows.

--- Samples/Nivaes.App.Cross.Mobile.Windows.Sample/Program.cs
+++ Samples/Nivaes.App.Cross.Mobile.Windows.Sample/Program.cs
@@ -1,12 +1,12 @@
-﻿namespace Nivaes.App.Cross.Mobele.Windows.Sample
-{
-    public static class Program
-    {
-        public static void Main(string[] args)
-        {
-            WinRT.ComWrappersSupport.InitializeComWrappers();
+﻿//namespace Nivaes.App.Cross.Mobele.Windows.Sample
+//{
+//    public static class Program
+//    {
+//        public static void Main(string[] args)
+//        {
+//            WinRT.ComWrappersSupport.InitializeComWrappers();
 
-            Microsoft.UI.Xaml.Application.Start(_ => new Mobile.Windows.Sample.App());
-        }
-    }
-}
+//            Microsoft.UI.Xaml.Application.Start(_ => new Mobile.Windows.Sample.App());
+//        }
+//    }
+//}
