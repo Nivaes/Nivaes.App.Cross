eff7398fc6e8b3ebf05a1d1fdf60f05265d9ec97
Andres Gimenez Muñoz <andres.gimenez@outlook.com>
2024-12-31 14:50:07 +0100

    RootViewfor mac

---
+++ samples/Nivaes.App.Cross.AppKit.MacOS.Sample/Components/Views/RootView.cs
@@ -0,0 +1,15 @@
+﻿namespace Nivaes.App.Cross.UIKit.Sample
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using Nivaes.App.Cross.AppKit;
+    using Nivaes.App.Cross.Sample;
+
+    internal class RootView
+        : CrossUIViewController<RootViewModel>
+    {
+    }
+}
