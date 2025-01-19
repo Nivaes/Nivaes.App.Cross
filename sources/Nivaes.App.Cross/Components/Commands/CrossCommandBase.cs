b6378c03eaaaf9249b64387e12c0515a8f3bb113
Andres Gimenez Muñoz <andres.gimenez@outlook.com>
2025-01-19 10:26:49 +0100

    Command

---
+++ sources/Nivaes.App.Cross/Components/Commands/CrossCommandBase.cs
@@ -0,0 +1,34 @@
+﻿namespace Nivaes.App.Cross
+{
+    public abstract class CrossCommandBase
+        : ICrossCommand
+    {
+        #region CanExecuteChanged
+        private readonly List<WeakReference> canExecuteChangedEventListeners = [];
+
+        /// <summary>Occurs when changes occur that affect whether or not the command should execute.</summary>
+        public event EventHandler? CanExecuteChanged
+        {
+            add 
+            {
+                canExecuteChangedEventListeners.Add(new WeakReference(value));
+            }
+            remove
+            {
+                canExecuteChangedEventListeners.RemoveAll(wr =>
+                {
+                    var target = wr.Target as EventHandler;
+                    if (target == null)
+                        return false;
+                    else
+                        return target == value;
+                });
+            }
+        }
+        #endregion
+
+        public abstract bool CanExecute(object? parameter);
+
+        public abstract void Execute(object? parameter);
+    }
+}
---
+++ sources/Nivaes.App.Cross/Components/Commands/ICrossCommand.cs
@@ -0,0 +1,12 @@
+﻿namespace Nivaes.App.Cross
+{
+    using System.Windows.Input;
+
+    /// <summary>Interface for delegate command.</summary>
+    public interface ICrossCommand 
+        : ICommand
+    {
+        ///// <summary>Raises the <see cref="ICommand.CanExecuteChanged"/> event.</summary>
+        //void RaiseCanExecuteChanged();
+    }
+}
