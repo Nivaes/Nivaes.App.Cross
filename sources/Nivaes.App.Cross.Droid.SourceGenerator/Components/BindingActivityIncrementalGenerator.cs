9b5a28b627535dc7ef7b11e8dcda9af3426d0ff0
Andres Gimenez Muñoz <andres.gimenez@outlook.com>
2025-11-08 13:09:46 +0100

    Recomposition

--- sources/Nivaes.App.Cross.Droid.SourceGenerator/Components/BindingActivityIncrementalGenerator.cs
+++ sources/Nivaes.App.Cross.Droid.SourceGenerator/Components/BindingActivityIncrementalGenerator.cs
@@ -258,7 +258,6 @@ namespace {action.containerType?.ContainingNamespace}
             context.AddSource(sourceName + "_IoCServiceContainer", source);
         }
 
-
         private static string ResolveConstructor(SourceProductionContext context, ServiceEntry entry, HashSet<string> transients)
         {
             var implementation = entry.Implementation;
