1f65f13f6edb34a39b5f8e0ff04acd7608275e82
Andres Gimenez Muñoz <andres.gimenez@outlook.com>
2025-12-28 12:39:21 +0100

    Mark IoC with obsolete

--- sources/Nivaes.App.Cross/Components/Mvx.cs
+++ sources/Nivaes.App.Cross/Components/Mvx.cs
@@ -3,11 +3,13 @@ namespace MvvmCross
     using MvvmCross.IoC;
     using Nivaes.App.Cross;
 
+    [Obsolete("Quitar MvxIoC")]
     public static class Mvx
     {
         /// <summary>
         /// Returns a singleton instance of the default IoC Provider. If possible use dependency injection instead.
         /// </summary>
+        [Obsolete("Quitar MvxIoC")]
         public static IMvxIoCProvider? IoCProvider => CrossSingleton<IMvxIoCProvider>.Instance;
     }
 }
