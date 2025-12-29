408c65edca87ec828c39e9f4919938b3d3a95e2f
Andres Gimenez Muñoz <andres.gimenez@outlook.com>
2025-12-29 14:34:50 +0100

    Change tu extension

--- sources/Nivaes.App.Cross/Components/Navigation/CrossNavigationExtensions.cs
+++ sources/Nivaes.App.Cross/Components/Navigation/CrossNavigationExtensions.cs
@@ -4,110 +4,105 @@ namespace Nivaes.App.Cross
 
     public static class CrossNavigationExtensions
     {
-        /// <summary>
-        /// Verifies if the provided Uri can be routed to a ViewModel request.
-        /// </summary>
-        /// <param name="navigationService"></param>
-        /// <param name="path">URI to route</param>
-        /// <returns>True if the uri can be routed or false if it cannot.</returns>
-        public static Task<bool> CanNavigate(this ICrossNavigationService navigationService, Uri path)
+        extension(ICrossNavigationService navigationService)
         {
-            return navigationService.CanNavigate(path.ToString());
-        }
+            /// <summary>
+            /// Verifies if the provided Uri can be routed to a ViewModel request.
+            /// </summary>
+            /// <param name="path">URI to route</param>
+            /// <returns>True if the uri can be routed or false if it cannot.</returns>
+            public Task<bool> CanNavigate(Uri path)
+            {
+                return navigationService.CanNavigate(path.ToString());
+            }
 
-        /// <summary>
-        /// Translates the provided Uri to a ViewModel request and dispatches it.
-        /// </summary>
-        /// <param name="navigationService"></param>
-        /// <param name="path">URI to route</param>
-        /// <param name="presentationBundle"></param>
-        /// <param name="cancellationToken"></param>
-        /// <returns>A task to await upon</returns>
-        [RequiresUnreferencedCode("This method uses reflection which may not be preserved during trimming")]
-        public static Task Navigate(this ICrossNavigationService navigationService, Uri path, ICrossBundle? presentationBundle = null, CancellationToken cancellationToken = default)
-        {
-            return navigationService.Navigate(path.ToString(), presentationBundle, cancellationToken);
-        }
+            /// <summary>
+            /// Translates the provided Uri to a ViewModel request and dispatches it.
+            /// </summary>
+            /// <param name="path">URI to route</param>
+            /// <param name="presentationBundle"></param>
+            /// <param name="cancellationToken"></param>
+            /// <returns>A task to await upon</returns>
+            [RequiresUnreferencedCode("This method uses reflection which may not be preserved during trimming")]
+            public Task Navigate(Uri path, ICrossBundle? presentationBundle = null, CancellationToken cancellationToken = default)
+            {
+                return navigationService.Navigate(path.ToString(), presentationBundle, cancellationToken);
+            }
 
-        [RequiresUnreferencedCode("This method uses reflection which may not be preserved during trimming")]
-        public static Task Navigate<TParameter>(this ICrossNavigationService navigationService, Uri path, TParameter param, ICrossBundle? presentationBundle = null, CancellationToken cancellationToken = default)
-        {
-            return navigationService.Navigate(path.ToString(), param, presentationBundle, cancellationToken);
-        }
+            [RequiresUnreferencedCode("This method uses reflection which may not be preserved during trimming")]
+            public Task Navigate<TParameter>(Uri path, TParameter param, ICrossBundle? presentationBundle = null, CancellationToken cancellationToken = default)
+            {
+                return navigationService.Navigate(path.ToString(), param, presentationBundle, cancellationToken);
+            }
 
-        /// <summary>
-        /// Navigate from a Result Awaiting ViewModel to a Result Setting ViewModel determined by its type
-        /// </summary>
-        /// <param name="navigationService">Navigation service</param>
-        /// <param name="fromViewModel">Result Awaiting ViewModel</param>
-        /// <param name="resultViewModelManager">Result ViewModel Manager</param>
-        /// <param name="presentationBundle">(optional) presentation bundle</param>
-        /// <param name="cancellationToken">(optional) CancellationToken to cancel the navigation</param>
-        /// <typeparam name="TViewModel">Type of <see cref="ICrossResultSettingViewModel{TResult}"/></typeparam>
-        /// <typeparam name="TResult">Result awaited by Result Awaiting ViewModel and set by Result Setting ViewModel</typeparam>
-        /// <returns>Boolean indicating successful navigation</returns>
-        public static async Task<bool> NavigateRegisteringToResult<[DynamicallyAccessedMembers(DynamicallyAccessedMemberTypes.PublicConstructors)] TViewModel, TResult>(
-            this ICrossNavigationService navigationService,
-            ICrossResultAwaitingViewModel<TResult> fromViewModel,
-            ICrossResultViewModelManager resultViewModelManager,
-            ICrossBundle? presentationBundle = null,
-            CancellationToken cancellationToken = default)
-            where TViewModel : ICrossResultSettingViewModel<TResult>, ICrossViewModel
-        {
-            bool navigated = await navigationService.Navigate<TViewModel>(presentationBundle, cancellationToken);
-            if (navigated)
-                fromViewModel.RegisterToResult(resultViewModelManager);
-            return navigated;
-        }
+            /// <summary>
+            /// Navigate from a Result Awaiting ViewModel to a Result Setting ViewModel determined by its type
+            /// </summary>
+            /// <param name="fromViewModel">Result Awaiting ViewModel</param>
+            /// <param name="resultViewModelManager">Result ViewModel Manager</param>
+            /// <param name="presentationBundle">(optional) presentation bundle</param>
+            /// <param name="cancellationToken">(optional) CancellationToken to cancel the navigation</param>
+            /// <typeparam name="TViewModel">Type of <see cref="ICrossResultSettingViewModel{TResult}"/></typeparam>
+            /// <typeparam name="TResult">Result awaited by Result Awaiting ViewModel and set by Result Setting ViewModel</typeparam>
+            /// <returns>Boolean indicating successful navigation</returns>
+            public async Task<bool> NavigateRegisteringToResult<[DynamicallyAccessedMembers(DynamicallyAccessedMemberTypes.PublicConstructors)] TViewModel, TResult>(
+                ICrossResultAwaitingViewModel<TResult> fromViewModel,
+                ICrossResultViewModelManager resultViewModelManager,
+                ICrossBundle? presentationBundle = null,
+                CancellationToken cancellationToken = default)
+                where TViewModel : ICrossResultSettingViewModel<TResult>, ICrossViewModel
+            {
+                bool navigated = await navigationService.Navigate<TViewModel>(presentationBundle, cancellationToken);
+                if (navigated)
+                    fromViewModel.RegisterToResult(resultViewModelManager);
+                return navigated;
+            }
 
-        /// <summary>
-        /// Navigate from a Result Awaiting ViewModel to a Result Setting ViewModel determined by its type, with parameter
-        /// </summary>
-        /// <param name="navigationService">Navigation service</param>
-        /// <param name="fromViewModel">Result Awaiting ViewModel</param>
-        /// <param name="resultViewModelManager">Result ViewModel Manager</param>
-        /// <param name="parameter">ViewModel parameter</param>
-        /// <param name="presentationBundle">(optional) presentation bundle</param>
-        /// <param name="cancellationToken">(optional) CancellationToken to cancel the navigation</param>
-        /// <typeparam name="TViewModel">Type of <see cref="ICrossResultSettingViewModel{TResult}"/> and <see cref="ICrossViewModel{TParameter}"/></typeparam>
-        /// <typeparam name="TResult">Result awaited by Result Awaiting ViewModel and set by Result Setting ViewModel</typeparam>
-        /// <returns>Boolean indicating successful navigation</returns>
-        public static async Task<bool> NavigateRegisteringToResult<[DynamicallyAccessedMembers(DynamicallyAccessedMemberTypes.PublicConstructors)] TViewModel, TParameter, TResult>(
-            this ICrossNavigationService navigationService,
-            ICrossResultAwaitingViewModel<TResult> fromViewModel,
-            ICrossResultViewModelManager resultViewModelManager,
-            TParameter parameter,
-            ICrossBundle? presentationBundle = null,
-            CancellationToken cancellationToken = default)
-            where TViewModel : ICrossResultSettingViewModel<TResult>, ICrossViewModel<TParameter>
-        {
-            bool navigated = await navigationService.Navigate<TViewModel, TParameter>(parameter, presentationBundle, cancellationToken);
-            if (navigated)
-                fromViewModel.RegisterToResult(resultViewModelManager);
-            return navigated;
-        }
+            /// <summary>
+            /// Navigate from a Result Awaiting ViewModel to a Result Setting ViewModel determined by its type, with parameter
+            /// </summary>
+            /// <param name="fromViewModel">Result Awaiting ViewModel</param>
+            /// <param name="resultViewModelManager">Result ViewModel Manager</param>
+            /// <param name="parameter">ViewModel parameter</param>
+            /// <param name="presentationBundle">(optional) presentation bundle</param>
+            /// <param name="cancellationToken">(optional) CancellationToken to cancel the navigation</param>
+            /// <typeparam name="TViewModel">Type of <see cref="ICrossResultSettingViewModel{TResult}"/> and <see cref="ICrossViewModel{TParameter}"/></typeparam>
+            /// <typeparam name="TResult">Result awaited by Result Awaiting ViewModel and set by Result Setting ViewModel</typeparam>
+            /// <returns>Boolean indicating successful navigation</returns>
+            public async Task<bool> NavigateRegisteringToResult<[DynamicallyAccessedMembers(DynamicallyAccessedMemberTypes.PublicConstructors)] TViewModel, TParameter, TResult>(
+                ICrossResultAwaitingViewModel<TResult> fromViewModel,
+                ICrossResultViewModelManager resultViewModelManager,
+                TParameter parameter,
+                ICrossBundle? presentationBundle = null,
+                CancellationToken cancellationToken = default)
+                where TViewModel : ICrossResultSettingViewModel<TResult>, ICrossViewModel<TParameter>
+            {
+                bool navigated = await navigationService.Navigate<TViewModel, TParameter>(parameter, presentationBundle, cancellationToken);
+                if (navigated)
+                    fromViewModel.RegisterToResult(resultViewModelManager);
+                return navigated;
+            }
 
-        /// <summary>
-        /// Closes the View attached to the Result Setting ViewModel, with result
-        /// </summary>
-        /// <param name="navigationService">Navigation service</param>
-        /// <param name="viewModel">Result Setting ViewModel to close</param>
-        /// <param name="result">Result set by Result Setting ViewModel</param>
-        /// <param name="cancellationToken">(optional) CancellationToken to cancel the closing</param>
-        /// <typeparam name="TViewModel">Type of <see cref="ICrossResultSettingViewModel{TResult}"/></typeparam>
-        /// <typeparam name="TResult">Result set by Result Setting ViewModel</typeparam>
-        /// <returns></returns>
-        public static async Task<bool> CloseSettingResult<TViewModel, TResult>(
-            this ICrossNavigationService navigationService,
-            TViewModel viewModel,
-            TResult result,
-            CancellationToken cancellationToken = default)
-            where TViewModel : ICrossResultSettingViewModel<TResult>, ICrossViewModel
-        {
-            bool closed = await navigationService.Close(viewModel, cancellationToken);
-            if (closed)
-                viewModel.SetResult(result);
-            return closed;
+            /// <summary>
+            /// Closes the View attached to the Result Setting ViewModel, with result
+            /// </summary>
+            /// <param name="viewModel">Result Setting ViewModel to close</param>
+            /// <param name="result">Result set by Result Setting ViewModel</param>
+            /// <param name="cancellationToken">(optional) CancellationToken to cancel the closing</param>
+            /// <typeparam name="TViewModel">Type of <see cref="ICrossResultSettingViewModel{TResult}"/></typeparam>
+            /// <typeparam name="TResult">Result set by Result Setting ViewModel</typeparam>
+            /// <returns></returns>
+            public async Task<bool> CloseSettingResult<TViewModel, TResult>(
+                TViewModel viewModel,
+                TResult result,
+                CancellationToken cancellationToken = default)
+                where TViewModel : ICrossResultSettingViewModel<TResult>, ICrossViewModel
+            {
+                bool closed = await navigationService.Close(viewModel, cancellationToken);
+                if (closed)
+                    viewModel.SetResult(result);
+                return closed;
+            }
         }
     }
 }
