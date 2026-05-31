3748ac6493a6e836d17be7da1eab531a7c4bc3b1
Andres Gimenez Muñoz <andres.gimenez@outlook.com>
2026-05-31 21:44:27 +0200

    Register Views

--- samples/Nivaes.App.Cross.Sample.WinUI/CrossProgram.cs
+++ samples/Nivaes.App.Cross.Sample.WinUI/CrossProgram.cs
@@ -16,23 +16,6 @@ public static class CrossProgram
         builder
             .UseSharedCrossApp();
 
-        /* ToDo: Cargar esto con roslyn */
-        builder.Services.AddScoped<BaseViewModel>();
-        builder.Services.AddScoped<MainViewModel>();
-        builder.Services.AddScoped<NewWindowViewModel>();
-        builder.Services.AddScoped<RootViewModel>();
-
-        builder.Services.AddScoped<ChildViewModel>();
-        builder.Services.AddScoped<ChildWithResultViewModel>();
-        builder.Services.AddScoped<FragmentCloseViewModel>();
-        builder.Services.AddScoped<WindowViewModel>();
-        builder.Services.AddScoped<WindowChildViewModel>();
-        builder.Services.AddScoped<TabsRootViewModel>();
-        builder.Services.AddScoped<TabsRootBViewModel>();
-        builder.Services.AddScoped<Tab1ViewModel>();
-        builder.Services.AddScoped<Tab2ViewModel>();
-        builder.Services.AddScoped<Tab3ViewModel>();
-
         return builder.Build();
     }
 }
