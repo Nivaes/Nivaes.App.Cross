d67d1d3d0bb4fa280f3c01f40613ec43a2fbf970
Andres Gimenez Muñoz <andres.gimenez@outlook.com>
2026-07-01 11:56:12 +0200

    Rename directories

--- run-ios-watch.sh
+++ run-ios-watch.sh
@@ -29,4 +29,4 @@ fi
 
 # 4. Ejecutar dotnet watch con Hot Reload
 echo "Iniciando dotnet watch con Hot Reload..."
-dotnet watch samples/Nivaes.App.Cross.Sample.UIKit.iOS  -f $TARGET_FRAMEWORK run --property:RuntimeIdentifier=$RUNTIME_ID
+dotnet watch Samples/Nivaes.App.Cross.Sample.UIKit.iOS  -f $TARGET_FRAMEWORK run --property:RuntimeIdentifier=$RUNTIME_ID
