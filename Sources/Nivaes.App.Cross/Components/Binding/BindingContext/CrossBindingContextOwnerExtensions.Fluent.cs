a3ca69c86e4ea9f74ba376816bbba759d76b92e0
Andres Gimenez Muñoz <andres.gimenez@outlook.com>
2026-07-04 15:53:47 +0200

    Compile

--- Sources/Nivaes.App.Cross/Components/Binding/BindingContext/CrossBindingContextOwnerExtensions.Fluent.cs
+++ Sources/Nivaes.App.Cross/Components/Binding/BindingContext/CrossBindingContextOwnerExtensions.Fluent.cs
@@ -4,28 +4,23 @@ namespace Nivaes.App.Cross;
 
 public static partial class CrossBindingContextOwnerExtensions
 {
-    public static CrossFluentBindingDescriptionSet<TTarget, TSource> CreateBindingSet<
-        [DynamicallyAccessedMembers(DynamicallyAccessedMemberTypes.Interfaces)] TTarget, TSource>(
-            this TTarget target)
-                where TTarget : class, ICrossBindingContextOwner
+    extension<[DynamicallyAccessedMembers(DynamicallyAccessedMemberTypes.Interfaces)] TTarget>(TTarget target) where TTarget : class, ICrossBindingContextOwner
     {
-        return new CrossFluentBindingDescriptionSet<TTarget, TSource>(target);
-    }
+        public CrossFluentBindingDescriptionSet<TTarget, TSource> CreateBindingSet<TSource>()
+        {
+            return new CrossFluentBindingDescriptionSet<TTarget, TSource>(target);
+        }
 
-    public static CrossFluentBindingDescriptionSet<TTarget, TSource> CreateBindingSet<
-        [DynamicallyAccessedMembers(DynamicallyAccessedMemberTypes.Interfaces)] TTarget, TSource>(
-            this TTarget target, string clearBindingKey)
-                where TTarget : class, ICrossBindingContextOwner
-    {
-        return new CrossFluentBindingDescriptionSet<TTarget, TSource>(target, clearBindingKey);
-    }
+        public CrossFluentBindingDescriptionSet<TTarget, TSource> CreateBindingSet<TSource>(string clearBindingKey)
+        {
+            return new CrossFluentBindingDescriptionSet<TTarget, TSource>(target, clearBindingKey);
+        }
 
-    public static MvxFluentBindingDescription<TTarget> CreateBinding<
-        [DynamicallyAccessedMembers(DynamicallyAccessedMemberTypes.Interfaces)] TTarget>(
-        this TTarget target)
-            where TTarget : class, ICrossBindingContextOwner
-    {
-        return new MvxFluentBindingDescription<TTarget>(target, target);
+        public MvxFluentBindingDescription<TTarget> CreateBinding(
+    )
+        {
+            return new MvxFluentBindingDescription<TTarget>(target, target);
+        }
     }
 
     public static MvxFluentBindingDescription<TTarget> CreateBinding<
