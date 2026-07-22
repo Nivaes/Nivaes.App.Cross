45c2b3ad2e833525aca7b04c8812ab137ada9048
Andres Gimenez Muñoz <andres.gimenez@outlook.com>
2026-07-22 09:58:26 +0200

    Fix Dispacher

--- Sources/Nivaes.App.Cross.WinUI/Components/Hosting/AppHostBuilderExtensions.cs
+++ Sources/Nivaes.App.Cross.WinUI/Components/Hosting/AppHostBuilderExtensions.cs
@@ -18,7 +18,12 @@ namespace Nivaes.App.Cross.WinUI
         static CrossAppBuilder SetupDefaults(this CrossAppBuilder builder, CrossWinUIApplication app)
         {
             // ToDo: Unificar interfaces.
-            builder.Services.TryAddSingleton<CrossWindowsViewDispatcher, CrossWindowsViewDispatcher>();
+            builder.Services.TryAddSingleton<CrossWindowsViewDispatcher>();
+            builder.Services.TryAddSingleton<ICrossViewDispatcher>(sp =>
+                sp.GetRequiredService<CrossWindowsViewDispatcher>());
+            builder.Services.TryAddSingleton<ICrossMainThreadDispatcher>(sp =>
+                sp.GetRequiredService<CrossWindowsViewDispatcher>());
+
 
             builder.Services.TryAddSingleton<ICrossWindowsFrame>(sp => new CrossWindowsFrame(app.RootFrame!));
             builder.Services.TryAddSingleton<IWindowsViewPresenterManager, MultiWindowViewPresenterManager>();
--- Sources/Nivaes.App.Cross.WinUI/Components/Views/CrossWindowsViewDispatcher.cs
+++ Sources/Nivaes.App.Cross.WinUI/Components/Views/CrossWindowsViewDispatcher.cs
@@ -2,6 +2,7 @@ using Microsoft.Extensions.Logging;
 
 namespace Nivaes.App.Cross.WinUI;
 
+// ToDo: unificar CrossWindowsViewDispatcher con CrossWindowsMainThreadDispatcher
 public class CrossWindowsViewDispatcher
     : CrossWindowsMainThreadDispatcher, ICrossViewDispatcher
 {
