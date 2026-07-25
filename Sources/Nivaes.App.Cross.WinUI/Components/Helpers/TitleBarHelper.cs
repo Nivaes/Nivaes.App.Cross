05658ef348f93d9485ddddbc8ed24ca8bdfd842a
Andres Gimenez Muñoz <andres.gimenez@outlook.com>
2026-07-25 15:57:37 +0200

    Update winui sdk

---
+++ Sources/Nivaes.App.Cross.WinUI/Components/Helpers/TitleBarHelper.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.UI;
+using Microsoft.UI.Xaml;
+using Windows.UI;
+
+namespace Nivaes.App.Cross.WinUI
+{
+    internal static class TitleBarHelper
+    {
+        public static void ApplySystemThemeToCaptionButtons(Window window, ElementTheme currentTheme)
+        {
+            if (window.AppWindow != null)
+            {
+                var foregroundColor = currentTheme == ElementTheme.Dark ? Colors.White : Colors.Black;
+                window.AppWindow.TitleBar.ButtonForegroundColor = foregroundColor;
+                window.AppWindow.TitleBar.ButtonHoverForegroundColor = foregroundColor;
+
+                var backgroundHoverColor = currentTheme == ElementTheme.Dark ? Color.FromArgb(24, 255, 255, 255) : Windows.UI.Color.FromArgb(24, 0, 0, 0);
+                window.AppWindow.TitleBar.ButtonHoverBackgroundColor = backgroundHoverColor;
+            }
+        }
+    }
+}
---
+++ Sources/Nivaes.App.Cross.WinUI/Components/Helpers/WindowHelper.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.UI.Windowing;
+using Microsoft.UI.Xaml;
+
+namespace Nivaes.App.Cross.WinUI
+{
+    internal static class WindowHelper
+    {
+        static public void SetWindowMinSize(this Window window, double width, double height)
+        {
+            if (window.Content is not FrameworkElement windowContent)
+            {
+                System.Diagnostics.Debug.WriteLine("Window content is not a FrameworkElement.");
+                return;
+            }
+
+            if (windowContent.XamlRoot is null)
+            {
+                System.Diagnostics.Debug.WriteLine("Window content's XamlRoot is null.");
+                return;
+            }
+
+            if (window.AppWindow.Presenter is not OverlappedPresenter presenter)
+            {
+                System.Diagnostics.Debug.WriteLine("Window's AppWindow.Presenter is not an OverlappedPresenter.");
+                return;
+            }
+
+            var scale = windowContent.XamlRoot.RasterizationScale;
+            var minWidth = width * scale;
+            var minHeight = height * scale;
+            presenter.PreferredMinimumWidth = (int)minWidth;
+            presenter.PreferredMinimumHeight = (int)minHeight;
+        }
+    }
+}
