07b8946e09e9462e8c3edd8b3ff06efc3c0c9e43
Andres Gimenez Muñoz <andres.gimenez@outlook.com>
2026-07-27 23:43:48 +0200

    Animation

--- Sources/Nivaes.App.Cross.WinUI/Components/UI/ListViewBaseBehavior.cs
+++ Sources/Nivaes.App.Cross.WinUI/Components/UI/ListViewBaseBehavior.cs
@@ -11,21 +11,19 @@ namespace Nivaes.App.Cross.WinUI
     public class ListViewBaseBehavior
         : Behavior<ListViewBase>
     {
+        public static string AnimatedKey = "_ForwardConnectedAnimation";
+
         protected override void OnAttached()
         {
             base.OnAttached();
 
             base.AssociatedObject.IsItemClickEnabled = true;
             base.AssociatedObject.ItemClick += OnItemClick;
-            //base.AssociatedObject.Tapped += OnTaped;
-            //base.AssociatedObject.SelectionChanged += OnSelectedItenChanged;
         }
 
         protected override void OnDetaching()
         {
             base.AssociatedObject.ItemClick -= OnItemClick;
-            //base.AssociatedObject.SelectionChanged -= OnSelectedItenChanged;
-            //base.AssociatedObject.Tapped -= OnTaped;
 
             base.OnDetaching();
         }
@@ -81,7 +79,7 @@ namespace Nivaes.App.Cross.WinUI
 
                         ConnectedAnimationService
                             .GetForCurrentView()
-                            .PrepareToAnimate("ForwardConnectedAnimation", image);
+                            .PrepareToAnimate(AnimatedKey, image);
                     }
 
                     if (command.CanExecute(selectedItem))
@@ -93,86 +91,14 @@ namespace Nivaes.App.Cross.WinUI
         }
         #endregion
 
-        //#region SelectedItemCommand
-        //public ICommand SelectedItemCommand
-        //{
-        //    get => (ICommand)base.GetValue(SelectedItemCommandProperty);
-        //    set => base.SetValue(SelectedItemCommandProperty, value);
-        //}
+        public static void AnimationStart(UIElement destination, IEnumerable<UIElement> coordinatedElements)
+        {
+            ConnectedAnimation imageAnimation = ConnectedAnimationService.GetForCurrentView().GetAnimation(AnimatedKey);
+            if (imageAnimation != null)
+            {
+                imageAnimation.TryStart(destination, coordinatedElements);
 
-        //public static readonly DependencyProperty SelectedItemCommandProperty =
-        //    DependencyProperty.RegisterAttached(
-        //        nameof(SelectedItemCommand),
-        //        typeof(ICommand),
-        //        typeof(SelectorBehavior),
-        //        new PropertyMetadata(null));
-
-        //private DateTime _lastSelectedItenAction = DateTime.UtcNow;
-
-        //private void OnSelectedItenChanged(object sender, SelectionChangedEventArgs e)
-        //{
-        //    if (_lastSelectedItenAction < DateTime.UtcNow)
-        //    {
-        //        var command = SelectedItemCommand;
-
-        //        if (command != null)
-        //        {
-        //            //var container = (SelectorItem)base.AssociatedObject.ContainerFromItem(e.AddedItems);
-        //            //var image = container.FindControl<Image>();
-        //            //ConnectedAnimationService
-        //            //    .GetForCurrentView()
-        //            //    .PrepareToAnimate("ForwardConnectedAnimation", image);
-
-        //            var selectedItem = base.AssociatedObject.SelectedItem;
-
-        //            if (command.CanExecute(selectedItem))
-        //                command.Execute(selectedItem);
-
-        //            _lastSelectedItenAction = DateTime.UtcNow.AddSeconds(0.5);
-        //        }
-        //    }
-        //}
-        //#endregion
-
-        //#region TapedCommand
-        //public ICommand TapedCommand
-        //{
-        //    get => (ICommand)base.GetValue(TapedCommandProperty);
-        //    set => base.SetValue(TapedCommandProperty, value);
-        //}
-
-        //public static readonly DependencyProperty TapedCommandProperty =
-        //    DependencyProperty.RegisterAttached(
-        //        nameof(TapedCommand),
-        //        typeof(ICommand),
-        //        typeof(Selector),
-        //        new PropertyMetadata(null));
-
-        //private DateTime _lastTapedAction = DateTime.UtcNow;
-        //private static object? _lastSelectedObject;
-
-        //private void OnTaped(object sender, TappedRoutedEventArgs e)
-        //{
-        //    if (_lastTapedAction < DateTime.UtcNow)
-        //    {
-        //        var command = TapedCommand;
-
-        //        if (command != null)
-        //        {
-        //            var selectedItem = base.AssociatedObject.SelectedItem;
-
-        //            if (_lastSelectedObject != selectedItem)
-        //            {
-        //                _lastSelectedObject = selectedItem;
-
-        //                if (command.CanExecute(selectedItem))
-        //                    command.Execute(selectedItem);
-        //            }
-
-        //            _lastTapedAction = DateTime.UtcNow.AddSeconds(0.5);
-        //        }
-        //    }
-        //}
-        //#endregion
+            }
+        }
     }
 }
