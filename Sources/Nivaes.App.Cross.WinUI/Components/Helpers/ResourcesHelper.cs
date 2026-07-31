8e424b7b84951aba259552904f4a6aaad98bb046
Andres Gimenez Muñoz <andres.gimenez@outlook.com>
2026-07-31 20:29:11 +0200

    Clone PathGeometry

--- Sources/Nivaes.App.Cross.WinUI/Components/Helpers/ResourcesHelper.cs
+++ Sources/Nivaes.App.Cross.WinUI/Components/Helpers/ResourcesHelper.cs
@@ -13,24 +13,79 @@ namespace Nivaes.App.Cross.WinUI
 
         public static IconElement? GetIcon(string resourceName)
         {
-            var icon = (PathIcon)Application.Current.Resources[resourceName];
-
-            PathGeometry geometry = (PathGeometry)icon.Data;
-
             try
             {
+                var icon = (PathIcon)Application.Current.Resources[resourceName];
+                var source = (PathGeometry)icon.Data;
+
                 return new PathIcon
                 {
-                    Data = new PathGeometry
-                    {
-                        Figures = geometry.Figures
-                    }
+                    Data = ClonePathGeometry(source)
                 };
             }
-            catch
+            catch(Exception ex)
             {
-                return null;
+                throw new AppException(ex, $"Icon {resourceName} does not exist");
             }
+        }
+
+        private static PathGeometry ClonePathGeometry(PathGeometry source)
+        {
+            var geometry = new PathGeometry { FillRule = source.FillRule };
+
+            foreach (var figure in source.Figures)
+            {
+                var newFigure = new PathFigure
+                {
+                    StartPoint = figure.StartPoint,
+                    IsClosed = figure.IsClosed,
+                    IsFilled = figure.IsFilled
+                };
+
+                foreach (var segment in figure.Segments)
+                {
+                    switch (segment)
+                    {
+                        case LineSegment line:
+                            newFigure.Segments.Add(new LineSegment { Point = line.Point });
+                            break;
+                        case BezierSegment bezier:
+                            newFigure.Segments.Add(new BezierSegment
+                            {
+                                Point1 = bezier.Point1,
+                                Point2 = bezier.Point2,
+                                Point3 = bezier.Point3
+                            });
+                            break;
+                        case ArcSegment arc:
+                            newFigure.Segments.Add(new ArcSegment
+                            {
+                                Point = arc.Point,
+                                Size = arc.Size,
+                                RotationAngle = arc.RotationAngle,
+                                IsLargeArc = arc.IsLargeArc,
+                                SweepDirection = arc.SweepDirection
+                            });
+                            break;
+                        case PolyLineSegment poly:
+                            var newPoly = new PolyLineSegment();
+                            foreach (var pt in poly.Points) newPoly.Points.Add(pt);
+                            newFigure.Segments.Add(newPoly);
+                            break;
+                        case QuadraticBezierSegment quad:
+                            newFigure.Segments.Add(new QuadraticBezierSegment
+                            {
+                                Point1 = quad.Point1,
+                                Point2 = quad.Point2
+                            });
+                            break;
+                    }
+                }
+
+                geometry.Figures.Add(newFigure);
+            }
+
+            return geometry;
         }
 
         public static Canvas GetCanvas(string resourceName)
